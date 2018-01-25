af81b05817298134bedd6579bd9bae38c6765b5d
Jeffrey Eppinger <eppinger@cmu.edu>
2018-01-25 12:20:29 -0500

    Upgraded to Django 1.11.
    Also removed admin references in generated urls.py to simplify demo.

--- webapps/urls.py
+++ webapps/urls.py
@@ -1,7 +1,7 @@
 """webapps URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/1.10/topics/http/urls/
+    https://docs.djangoproject.com/en/1.11/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -14,13 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 from django.conf.urls import url
-from django.contrib import admin
 
 from intro import views
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    
     url(r'^$', views.home_page),
     url(r'^hello-world$', views.hello_world),
     url(r'^hello-world-with-template$', views.hello_world_with_template),
