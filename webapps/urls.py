cb9575578017a581c79f302f1abe00cf3f27eb82
Jeffrey Eppinger <eppinger@cmu.edu>
2019-01-23 11:42:39 -0500

    Upgraded to Django 2.1.

--- webapps/urls.py
+++ webapps/urls.py
@@ -1,25 +1,24 @@
 """webapps URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/1.11/topics/http/urls/
+    https://docs.djangoproject.com/en/2.1/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
 Class-based views
     1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
 Including another URLconf
-    1. Import the include() function: from django.conf.urls import url, include
-    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.conf.urls import url
-
+from django.urls import path
 from intro import views
 
 urlpatterns = [
-    url(r'^$', views.home_page),
-    url(r'^hello-world$', views.hello_world),
-    url(r'^hello-world-with-template$', views.hello_world_with_template),
-    url(r'^greet$', views.greet),
+    path('', views.home_page),
+    path('hello-world', views.hello_world),
+    path('hello-world-with-template', views.hello_world_with_template),
+    path('greet', views.greet),
 ]
